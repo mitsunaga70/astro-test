62f7098423619cda9b01a46a1d89d78db6054ecb
mitsunaga <mitsunaga@fieldweb.co.jp>
2023-09-20 17:41:28 +0900

    #

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,14 @@
 import { defineConfig } from 'astro/config';
 import postcssMergeQueries from "postcss-merge-queries";
-import relativeLinks from 'astro-relative-links';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+
 // https://astro.build/config
 export default defineConfig({
+  integrations: [sitemap({
+    lastmod: new Date(),
+  })],
+
   vite: {
-    resolve: {
-      alias: {
-        '@/': `${path.resolve(__dirname, 'src')}/`
-      }
-    },
     css: {
       postcss: {
         plugins: [postcssMergeQueries]
@@ -25,34 +19,7 @@ export default defineConfig({
         }
       }
     },
-    build: {
-
-      assetsInlineLimit: 0,
-      rollupOptions: {
-        output: {
-          assetFileNames: assetInfo => {
-            let extType = assetInfo.name.split('.')[1];
-            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-              extType = 'img';
-            } else if (/woff|woff2/.test(extType)) {
-              extType = 'css';
-            }
-            return `assets/${extType}/[name]-[hash][extname]`;
-          },
-          chunkFileNames: 'assets/js/[name]-[hash].js',
-          entryFileNames: 'assets/js/[name]-[hash].js'
-        }
-      }
-    },
-
   },
-  integrations: [relativeLinks(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), sitemap({
-    lastmod: new Date(),
-  }),],
   server: {
     host: "192.168.30.28",
     port: 8080,
